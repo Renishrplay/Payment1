8962927b06ffdf1a2c2cc2d3516f71a0968c84f3
Rplay <americarplay@gmail.com>
2023-08-20 06:10:34 +0100

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -372,31 +372,39 @@ async def manual_filters(client, message, text=False):
                 try:
                     if fileid == "None":
                         if btn == "[]":
-                            await client.send_message(group_id, reply_text, disable_web_page_preview=True, reply_to_message_id=reply_id)
+                            menukbd3 = await client.send_message(group_id, reply_text, disable_web_page_preview=True, reply_to_message_id=reply_id)
+                            await asyncio.sleep(IMDB_DELET_TIME)
+                            await menukbd3.delete()
                         else:
                             button = eval(btn)
-                            await client.send_message(
+                            menukbd2 = await client.send_message(
                                 group_id,
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button),
                                 reply_to_message_id=reply_id
                             )
+                            await asyncio.sleep(IMDB_DELET_TIME)
+                            await menukbd2.delete()
                     elif btn == "[]":
-                        await client.send_cached_media(
+                        menukbd1 = await client.send_cached_media(
                             group_id,
                             fileid,
                             caption=reply_text or "",
                             reply_to_message_id=reply_id
                         )
+                        await asyncio.sleep(IMDB_DELET_TIME)
+                        await menukbd1.delete()
                     else:
                         button = eval(btn)
-                        await message.reply_cached_media(
+                        menukbd = await message.reply_cached_media(
                             fileid,
                             caption=reply_text or "",
                             reply_markup=InlineKeyboardMarkup(button),
                             reply_to_message_id=reply_id
                         )
+                        await asyncio.sleep(IMDB_DELET_TIME)
+                        await menukbd.delete()
                 except Exception as e:
                     logger.exception(e)
                 break
