1572ffa2355a76a570701477a03360aa757904a0
Rplay <americarplay@gmail.com>
2023-09-05 19:53:45 +0100

    Update carbon.py

--- plugins/ExraMods/carbon.py
+++ plugins/ExraMods/carbon.py
@@ -12,7 +12,7 @@ from re import findall
 from re import sub as re_sub
 from sys import executable
 import aiofiles
-import speedtest
+#import speedtest
 from PIL import Image
 from pyrogram.types import Message
 
