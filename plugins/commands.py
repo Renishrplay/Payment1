a3c6c4caaeb692f106c6e881736f75e6d8ca6a04
Rplay <americarplay@gmail.com>
2023-10-20 20:33:13 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -67,7 +67,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url="https://t.me/+hJ7dmFk_3N9mNTc1"
+                    "🤖 Join Updates Channel", url=invite_link.invite_link
                 )
             ]
         ]
