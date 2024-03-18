37df477833fd3ce48ad8d11770af98cbf6a48351
Rplay <americarplay@gmail.com>
2024-03-18 22:24:57 +0100

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -290,29 +290,29 @@ async def cb_handler(client: Client, query: CallbackQuery):
       
     elif query.data.startswith("checksub"):
         await ForceSub(client, query)
-    #    ident, file_id = query.data.split("#")
-   #     files_ = await get_file_details(file_id)
- #       if not files_:
-  #          return await query.answer('No such file exist.')
- #       files = files_[0]
- #       title = files.file_name
-#        size = get_size(files.file_size)
-  #      f_caption = files.caption
-  #      if CUSTOM_FILE_CAPTION:
- #           try:
- #              f_caption = CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name='' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)  
- #           except Exception as e:
-#                logger.exception(e)
- #               f_caption = f_caption
-  #      if f_caption is None:
-#            f_caption = f"{title}"
-#        await query.answer()
-#        await client.send_cached_media(
-#            chat_id=query.from_user.id,
-#            file_id=file_id,
- #           caption=f_caption,
-#            protect_content=True if ident == 'checksubp' else False
-#        )
+        ident, file_id = query.data.split("#")
+        files_ = await get_file_details(file_id)
+        if not files_:
+            return await query.answer('No such file exist.')
+        files = files_[0]
+        title = files.file_name
+        size = get_size(files.file_size)
+        f_caption = files.caption
+        if CUSTOM_FILE_CAPTION:
+            try:
+               f_caption = CUSTOM_FILE_CAPTION.format(mention=query.from_user.mention, file_name='' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)  
+            except Exception as e:
+                logger.exception(e)
+                f_caption = f_caption
+        if f_caption is None:
+            f_caption = f"{title}"
+        await query.answer()
+        await client.send_cached_media(
+            chat_id=query.from_user.id,
+            file_id=file_id,
+            caption=f_caption,
+            protect_content=True if ident == 'checksubp' else False
+        )
 
     elif query.data == "pages":
         await query.answer("🤨 Curiosity is a little more, isn't it? 😁", show_alert=True)
