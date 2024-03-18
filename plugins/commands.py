5f8152e14a367847d904a250da5f53791775f1b6
Rplay <americarplay@gmail.com>
2024-03-18 18:56:51 +0100

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -73,9 +73,8 @@ async def start(client, message):
             ]
         ]
 
-        if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help", "start", "hehe"]:
-        if message.command[1] == "subscribe":
-            await ForceSub(client, message)
+    if message.command[1] == "subscribe":
+        await ForceSub(client, message)
 
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
