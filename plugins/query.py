6f9284ae4e945d71ce3f3e00e89642c39884e8c1
Rplay <americarplay@gmail.com>
2024-05-27 23:19:25 +0100

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -300,8 +300,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(client, query):
             await query.answer("Jᴏɪɴ ᴏᴜʀ Bᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ ᴍᴀʜɴ! 😒", show_alert=True)
             return
-        ident, kk, file_id = query.data.split("#")
-        await query.answer(url=f"https://t.me/{temp.U_NAME}?start={kk}_{file_id}")
+        ident, file_id = query.data.split("#")
+        await query.answer(url=f"https://t.me/{temp.U_NAME}?start={file_id}")
 
     elif query.data == "pages":
         await query.answer("🤨 Curiosity is a little more, isn't it? 😁", show_alert=True)
