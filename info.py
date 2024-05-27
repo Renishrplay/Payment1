a61d3ca0511cb0a86d9226fa2bda094a36991e03
Rplay <americarplay@gmail.com>
2024-05-27 23:22:00 +0100

    Update info.py

--- info.py
+++ info.py
@@ -47,7 +47,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 REQ_CHANNEL = environ.get("REQ_CHANNEL", "-1002068064302")
-REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False
+REQ_CHANNEL = [int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL) else False]
 
 
 #maximum search result buttos count in number#
